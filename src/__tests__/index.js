19cd0b56f5453361028078c945fde8089b31159f
Shabith Ishan <shabith.ishan@gmail.com>
2017-06-03 01:07:20 +0530

    v0.0.1 ready

---
+++ src/__tests__/index.js
@@ -0,0 +1,114 @@
+import {nicInfo} from '../';
+const {describe, test, expect} = global;
+
+describe('Old NIC ID', () => {
+  test('should return information object', () => {
+    expect(nicInfo('853409999V')).not.toBeNull();
+    expect(nicInfo('907510999V')).not.toBeNull();
+  });
+
+  describe('Gender', () => {
+    test('should return correct Gender Type', () => {
+      expect(nicInfo('853409999V').gender.type).toEqual('M');
+      expect(nicInfo('907510999V').gender.type).toEqual('F');
+    });
+
+    test('should return correct Gender Label', () => {
+      expect(nicInfo('853409999V').gender.label).toEqual('Male');
+      expect(nicInfo('907510999V').gender.label).toEqual('Female');
+    });
+  });
+
+  describe('Birthday', () => {
+    test('should return correct Birth Year', () => {
+      expect(nicInfo('853409999V').birthday.year).toEqual(1985);
+      expect(nicInfo('907510999V').birthday.year).toEqual(1990);
+    });
+
+    test('should return correct Birth Month (0 based)', () => {
+      expect(nicInfo('853409999V').birthday.month).toEqual(11);
+      expect(nicInfo('907510999V').birthday.month).toEqual(8);
+    });
+
+    test('should return correct Birth date', () => {
+      expect(nicInfo('853409999V').birthday.date).toEqual(5);
+      expect(nicInfo('907510999V').birthday.date).toEqual(7);
+    });
+
+    test('should return momentjs object', () => {
+      expect(nicInfo('853409999V').birthday.momentjsObj).not.toBeNull();
+      expect(nicInfo('907510999V').birthday.momentjsObj).not.toBeNull();
+    });
+  });
+
+
+});
+
+describe('New NIC ID [Experimental]', () => {
+  test('should return information object', () => {
+    expect(nicInfo('198534099999')).not.toBeNull();
+  });
+
+  describe('Gender', () => {
+    test('should return correct Gender Type', () => {
+      expect(nicInfo('198534099999').gender.type).toEqual('M');
+    });
+
+    test('should return correct Gender Label', () => {
+      expect(nicInfo('198534099999').gender.label).toEqual('Male');
+    });
+  });
+
+  describe('Birthday', () => {
+    test('should return correct Birth Year', () => {
+      expect(nicInfo('198534099999').birthday.year).toEqual(1985);
+    });
+
+    test('should return correct Birth Month (0 based)', () => {
+      expect(nicInfo('198534099999').birthday.month).toEqual(11);
+    });
+
+    test('should return correct Birth date', () => {
+      expect(nicInfo('198534099999').birthday.date).toEqual(5);
+    });
+
+    test('should return momentjs object', () => {
+      expect(nicInfo('198534099999').birthday.momentjsObj).not.toBeNull();
+    });
+  });
+
+
+});
+
+
+// test('should convert single digits', () => {
+//   expect(numFormatter(1)).toBe('1');
+// });
+//
+// test('should convert double digits', () => {
+//   expect(numFormatter(12)).toBe('12');
+// });
+//
+// test('should convert triple digits', () => {
+//   expect(numFormatter(123)).toBe('123');
+// });
+//
+// test('should convert 4 digits', () => {
+//   expect(numFormatter(1234)).toBe('1,234');
+// });
+//
+// test('should convert 5 digits', () => {
+//   expect(numFormatter(12345)).toBe('12,345');
+// });
+//
+// test('should convert 6 digits', () => {
+//   expect(numFormatter(123456)).toBe('123,456');
+// });
+//
+// test('should convert 7 digits', () => {
+//   expect(numFormatter(1234567)).toBe('1,234,567');
+// });
+//
+// test('should convert 8 digits', () => {
+//   expect(numFormatter(12345678)).toBe('12,345,678');
+// });
---
+++ src/index.js
@@ -0,0 +1,79 @@
+import moment from 'moment';
+
+/**
+ * New NIC number format - issued from 1st of January 2016
+ * @param  {string} nicNo National Identity Card No.
+ * @return {object}       Gender and Birthday Information
+ */
+
+const newIDFormat = (nicNo) => {
+  let year = nicNo.slice(0, 4);
+  let days = nicNo.slice(4, 7);
+  let gender = { type: 'M', label: 'Male'};
+
+  if (days > 500) {
+    gender = { type: 'F', label: 'Female'};
+    days -= 500;
+  }
+
+  let birthdayMomentObj = moment(year + '-01-01').dayOfYear(days - 1);
+  let birthday = {
+    year: birthdayMomentObj.year(),
+    month: birthdayMomentObj.month(), // 0 based (0 to 11)
+    date: birthdayMomentObj.date(),
+    momentjsObj: birthdayMomentObj
+  };
+
+  return {
+    gender,
+    birthday
+  };
+};
+
+/**
+ * Old NIC number format - issued before 1st of Janurary 2016
+ * @param  {string} nicNo National Identity Card No.
+ * @return {object}       Gender and Birthday Information
+ */
+const oldIDFormat = (nicNo) => {
+  let year = nicNo.slice(0, 2);
+  let days = nicNo.slice(2, 5);
+  let gender = { type: 'M', label: 'Male'};
+
+  if (days > 500) {
+    gender.type = 'F';
+    gender.label = 'Female';
+    days -= 500;
+  }
+
+  let birthdayMomentObj = moment('19' + year + '-01-01').dayOfYear(days - 1);
+  let birthday = {
+    year: birthdayMomentObj.year(),
+    month: birthdayMomentObj.month(), // 0 based (0 to 11)
+    date: birthdayMomentObj.date(),
+    momentjsObj: birthdayMomentObj
+  };
+
+  return {
+    gender,
+    birthday
+  };
+};
+
+/**
+ * Get info by providing NIC No
+ * @param {string} NicNo
+ * @return {object}
+ */
+
+export const nicInfo = (nicNo) => {
+  //  check if it is new NIC or old NIC
+  let _return;
+  if (nicNo.length > 10) {
+    _return = newIDFormat(nicNo);
+  } else {
+    _return = oldIDFormat(nicNo);
+  }
+
+  return _return;
+};
